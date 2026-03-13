6ab89fb15bcac619828055f103d5056d8c0109e9
Caden Ng <cadenng2028@cdnis.edu.hk>
2026-03-13 13:24:59 +0800

    Commit 1 ( wasnt really 1 but yea)
    I added lots of variables

---
+++ StarterProjectMultipleViews/ViewControllerFour.swift
@@ -0,0 +1,23 @@
+//
+//  ViewControllerFour.swift
+//  StarterProjectMultipleViews
+//
+//  Created by Caden Ng on 4/3/2026.
+//
+
+import UIKit
+
+class ViewControllerFour: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
